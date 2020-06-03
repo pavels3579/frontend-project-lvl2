83bce2dcb09acfc80b334da22d3a24a6d46cea11
pavels3579 <pavels357@yandex.ru>
2020-06-03 22:17:46 +0400

    step1. feature(gendiff.js) add import commander

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,10 +1,10 @@
 // const program = require('commander');
+import program from 'commander';
 
 const gendiff = () => {
-  const {program} = require('commander');
-
   program
     .version('0.1.0')
+    .description('Compares two configuration files and shows a difference.')
     .parse(process.argv);
 };
 
