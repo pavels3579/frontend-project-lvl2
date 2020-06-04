a366a777d456b9bb4996c318d4c63395000c7d7d
pavels3579 <pavels357@yandex.ru>
2020-06-04 21:52:14 +0400

    step2. feature(gendiff.js) add format, arguments, option

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,6 +5,8 @@ const gendiff = () => {
   program
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
     .parse(process.argv);
 };
 
