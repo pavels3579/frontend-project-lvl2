d20636c7e4478287fe4480c78cfab2af6bdbeb51
pavels3579 <pavels357@yandex.ru>
2020-06-09 22:32:21 +0400

    step4. refactore

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,19 +2,22 @@ import { test, expect } from '@jest/globals';
 import getdiff from '../src/getdiff.js';
 
 test('gendiff', () => {
-  const result = '{\r\n\
-    host: hexlet.io\r\n\
-  + timeout: 20\r\n\
-  - timeout: 50\r\n\
-  - proxy: 123.234.53.22\r\n\
-  - follow: false\r\n\
-  + verbose: true\r\n\
-}';
+  const result = [
+    '{',
+    '    host: hexlet.io',
+    '  + timeout: 20',
+    '  - timeout: 50',
+    '  - proxy: 123.234.53.22',
+    '  - follow: false',
+    '  + verbose: true',
+    '}',
+  ];
 
   const dirname = process.cwd();
   // console.log('1', __dirname);
+  const newLine = '\r\n';
 
   const filepath1 = `${dirname}/__fixtures__/before.json`;
   const filepath2 = `${dirname}/__fixtures__/after.json`;
-  expect(getdiff(filepath1, filepath2)).toEqual(result);
+  expect(getdiff(filepath1, filepath2)).toEqual(result.join(newLine));
 });
--- src/getdiff.js
+++ src/getdiff.js
@@ -3,47 +3,44 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 
-const getdiff = (filepath1, filepath2) => {
-  let result = '';
+const getpath = (filePath) => {
   const workingDirectory = process.cwd();
+  let correctPath = filePath;
+
+  if (!correctPath.includes(workingDirectory)) {
+    correctPath = path.resolve(workingDirectory, correctPath);
+  }
+
+  return correctPath;
+};
+
+const getdiff = (filepath1, filepath2) => {
   const newLine = '\r\n';
-
-  let path1 = filepath1;
-  let path2 = filepath2;
-
-  if (!path1.includes(workingDirectory)) {
-    path1 = path.resolve(workingDirectory, path1);
-  }
-
-  if (!path2.includes(workingDirectory)) {
-    path2 = path.resolve(workingDirectory, path2);
-  }
-
-  const json1 = fs.readFileSync(path1, 'utf8');
+  const path1 = getpath(filepath1);
+  const path2 = getpath(filepath2);
+  const json1 = fs.readFileSync(path1);
   const obj1 = JSON.parse(json1);
   const json2 = fs.readFileSync(path2);
   const obj2 = JSON.parse(json2);
   const keys = _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]);
-
-  result = `{${newLine}`;
+  const result = ['{'];
 
   for (let i = 0; i < keys.length; i += 1) {
     if (_.has(obj2, keys[i]) && _.has(obj1, keys[i])) {
       if (obj2[keys[i]] === obj1[keys[i]]) {
-        result = `${result}    ${keys[i]}: ${obj1[keys[i]]}${newLine}`;
+        result.push(`    ${keys[i]}: ${obj1[keys[i]]}`);
       } else {
-        result = `${result}  + ${keys[i]}: ${obj2[keys[i]]}${newLine}`;
-        result = `${result}  - ${keys[i]}: ${obj1[keys[i]]}${newLine}`;
+        result.push(`  + ${keys[i]}: ${obj2[keys[i]]}`);
+        result.push(`  - ${keys[i]}: ${obj1[keys[i]]}`);
       }
     } else if (_.has(obj2, keys[i])) {
-      result = `${result}  + ${keys[i]}: ${obj2[keys[i]]}${newLine}`;
+      result.push(`  + ${keys[i]}: ${obj2[keys[i]]}`);
     } else {
-      result = `${result}  - ${keys[i]}: ${obj1[keys[i]]}${newLine}`;
+      result.push(`  - ${keys[i]}: ${obj1[keys[i]]}`);
     }
   }
-  result = `${result}}`;
-  // console.log(result);
-  return result;
+  result.push('}');
+  return result.join(newLine);
 };
 
 export default getdiff;
