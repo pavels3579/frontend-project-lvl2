b67b803e4361938c601857e6e7b62676374a61cd
pavels3579 <pavels357@yandex.ru>
2020-07-22 14:55:34 +0400

    step7. feature, add reduce

--- src/parsers.js
+++ src/parsers.js
@@ -3,22 +3,27 @@ import _ from 'lodash';
 const getParsing = (obj1, obj2) => {
   const newLine = '\r\n';
   const keys = _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]);
-  const result = ['{'];
 
-  for (let i = 0; i < keys.length; i += 1) {
-    if (_.has(obj2, keys[i]) && _.has(obj1, keys[i])) {
-      if (obj2[keys[i]] === obj1[keys[i]]) {
-        result.push(`    ${keys[i]}: ${obj1[keys[i]]}`);
-      } else {
-        result.push(`  + ${keys[i]}: ${obj2[keys[i]]}`);
-        result.push(`  - ${keys[i]}: ${obj1[keys[i]]}`);
-      }
-    } else if (_.has(obj2, keys[i])) {
-      result.push(`  + ${keys[i]}: ${obj2[keys[i]]}`);
-    } else {
-      result.push(`  - ${keys[i]}: ${obj1[keys[i]]}`);
+  const result = keys.reduce((acc, el) => {
+    if (obj2[el] === obj1[el]) {
+      acc.push(`    ${el}: ${obj2[el]}`);
+      return acc;
     }
-  }
+
+    if (obj2[el] !== undefined) {
+      acc.push(`  + ${el}: ${obj2[el]}`);
+    }
+
+    if (obj1[el] !== undefined) {
+      acc.push(`  - ${el}: ${obj1[el]}`);
+    }
+
+    return acc;
+  }, []);
+
+  // console.log('result: ', result);
+
+  result.unshift('{');
   result.push('}');
   return result.join(newLine);
 };
---
+++ src/stylish.js
@@ -0,0 +1,5 @@
+const stylish = (data) => {
+
+};
+
+export default stylish;
