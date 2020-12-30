960fe43a32724ec4b01662906d58a418f3e59918
pavels3579 <pavels357@yandex.ru>
2020-12-30 23:52:13 +0400

    refactore, change getAST

--- src/getAST.js
+++ src/getAST.js
@@ -7,27 +7,19 @@ const getAST = (data1, data2) => {
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
       const children = getAST(data1[key], data2[key]);
 
-      return {
-        key, type: 'nested', children,
-      };
+      return { key, type: 'nested', children };
     }
 
     if (_.isEqual(data1[key], data2[key])) {
-      return {
-        key, type: 'unchanged', value: data1[key],
-      };
+      return { key, type: 'unchanged', value: data1[key] };
     }
 
     if (!_.has(data1, key) && _.has(data2, key)) {
-      return {
-        key, type: 'added', value: data2[key],
-      };
+      return { key, type: 'added', value: data2[key] };
     }
 
     if (_.has(data1, key) && !_.has(data2, key)) {
-      return {
-        key, type: 'deleted', value: data1[key],
-      };
+      return { key, type: 'deleted', value: data1[key] };
     }
 
     return {
