54da5e74cdd279f344a9f9549fd24f716a56cc8d
pavels3579 <pavels357@yandex.ru>
2021-01-06 22:42:01 +0400

    check 8. change getAST

--- src/getAST.js
+++ src/getAST.js
@@ -4,12 +4,6 @@ const getAST = (data1, data2) => {
   const keys = _.union(Object.keys(data1), Object.keys(data2));
 
   const result = keys.map((key) => {
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      const children = getAST(data1[key], data2[key]);
-
-      return { key, type: 'nested', children };
-    }
-
     if (_.isEqual(data1[key], data2[key])) {
       return { key, type: 'unchanged', value: data1[key] };
     }
@@ -20,6 +14,12 @@ const getAST = (data1, data2) => {
 
     if (_.has(data1, key) && !_.has(data2, key)) {
       return { key, type: 'deleted', value: data1[key] };
+    }
+
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      const children = getAST(data1[key], data2[key]);
+
+      return { key, type: 'nested', children };
     }
 
     return {
